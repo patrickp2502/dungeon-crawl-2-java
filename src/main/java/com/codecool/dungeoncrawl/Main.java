e02824f30f2f70832f4d75615ff7ba764918366f
Fabio Tietz <fabiotietz0@gmail.com>
2022-06-03 07:39:28 +0200

    Before changing the file paths to load files from directory diynamicly

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -24,18 +24,25 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main extends Application {
     AssetCollection assetCollection = new AssetCollection();
-    //List<Scenery> scenery = assetCollection.separate(Scenery.class);
 
     EventEngine eventEngine;
     PhysEngine physEngine;
     MapLoader mapLoader = new MapLoader();
     String[] FILE_PATHS = {"/map.txt", "/map2.txt"};
-    GameMap map = mapLoader.loadMap(assetCollection, FILE_PATHS[1]);
+    GameMap gameMap = mapLoader.loadMap(assetCollection, FILE_PATHS[0]);
+
+    /*ArrayList<String> file_paths = new ArrayList<>();
+    file_paths.add("/map.txt");
+    file_paths.add("/map2.txt");
+
+    GameMap map = mapLoader.loadMap(assetCollection, file_paths.get(1));*/
     Canvas canvas = new Canvas(
             map.getWidth() * Tiles.TILE_WIDTH,
             map.getHeight() * Tiles.TILE_WIDTH);
